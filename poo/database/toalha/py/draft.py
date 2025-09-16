0b7781e347e1b4b1bea21b7c82370c1b912892b5
Lara Abreu <laraabreu1129@gmail.com>
2025-09-16 20:23:47 +0000

    atividade toalha. 2h

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,19 @@
+class Towel:
+    def __init__(self, color: str, size: str): #construtor
+        self.color = "red" #atributos
+        self.size = "p"
+        self.wetness = 0
+
+    def __str__(self) :
+        return f"color: {self.color}, tam:{self.size}, umi:{self.wetness}"
+
+#variavel ou referencia
+toalha = Towel("green", "g") #objetos
+toalha = Towel("red", "p")
+outra = toalha
+print(toalha.color)
+toalha.color = "white"
+outra.color = "blue"
+print(toalha.color)
+print(toalha.size)
+print(toalha.wetness)
