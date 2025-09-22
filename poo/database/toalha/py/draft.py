09b5412d0d7c3312e00a7c92e73fd9e2d17eeb09
Lara Abreu <laraabreu1129@gmail.com>
2025-09-22 16:24:33 -0300

    aula2 toalha

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,13 +1,38 @@
+
+
+
 class Towel:
-    def __init__(self, color: str, size: str): #construtor
+    def __init__(self, color: str, size: str): #construtor: padrao para definir que aquele metodo eh o construtor
         self.color = "red" #atributos
         self.size = "p"
+        self.wetness: int = 0
+
+    def dry(self, amount:int) -> None:  #amount: quantidade de agua
+        self.wetness += amount
+        if self.wetness >= self.getMaxWetness():
+            print("toalha encharcada")
+            self.wetness = self.getMaxWetness()
+
+    def isDry(self) -> bool:
+        return self.wetness == 0
+    
+    def wringOut(self) -> None:
         self.wetness = 0
 
-    def __str__(self) :
-        return f"color: {self.color}, tam:{self.size}, umi:{self.wetness}"
+    def getMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20
+        if self.size == "G":
+            return 30
+        return 0
+
+    def __str__(self) :  #transforma em texto, equivalente ao "toString"
+        return f"color: {self.color}, tam:{self.size}, umidade:{self.wetness}"  #return: devolve para quem solicitou
 
 #variavel ou referencia
+minha: Towel = Towel("red", "M")
 toalha = Towel("green", "g") #objetos
 toalha = Towel("red", "p")
 outra = toalha
@@ -17,5 +42,15 @@ outra.color = "blue"
 print(toalha.color)
 print(toalha.size)
 print(toalha.wetness)
+
+doguito = Towel("velha", "M")  #toalha foi criada aqui
+print(doguito)
+doguito.dry(3)
+print(doguito)
+doguito.dry(15)
+doguito.dry(10)
+print(doguito)
+
+
 
 #https://classroom.github.com/a/G161n2yI
